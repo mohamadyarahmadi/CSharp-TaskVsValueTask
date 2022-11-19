0fee8cb7830b2f0c5c3d6b2fb7a318c662525b41
DESKTOP-A2LDFJA\mohamad <mohamad.yarahmadiy@gmail.com>
2022-11-19 10:56:09 +0330

    initial app

---
+++ TaskVsValueTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using TaskVsValueTask.Services;
+
+namespace TaskVsValueTask
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
