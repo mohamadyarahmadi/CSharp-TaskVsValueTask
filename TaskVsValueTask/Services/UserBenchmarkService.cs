c08a5d0da8a6c9116316b851135d33b4fd47f7fc
DESKTOP-A2LDFJA\mohamad <mohamad.yarahmadiy@gmail.com>
2022-11-19 11:02:09 +0330

    add benchmark class and method

---
+++ TaskVsValueTask/Services/UserBenchmarkService.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TaskVsValueTask.Services;
+
+[MemoryDiagnoser]
+public class UserBenchmarkService
+{
+    [Benchmark]
+    public async Task RunTask()
+    {
+        var userService = new UserService();
+        var userNames = new string[] { "mohamad", "hasan", "hoseyn","moslem" };
+        for (int i = 0; i < 100; i++)
+        {
+            foreach (var userName in userNames)
+            {
+                var repo = await userService.GetUsersAsyncTask(userName);
+            }
+        }
+    }
+
+    [Benchmark]
+    public async Task RunValueTask()
+    {
+        var userService = new UserService();
+        var userNames = new string[] { "mohamad", "hasan", "hoseyn", "moslem" };
+        for (int i = 0; i < 100; i++)
+        {
+            foreach (var userName in userNames)
+            {
+                var repo = await userService.GetUsersAsyncValueTask(userName);
+            }
+        }
+    }
+}
